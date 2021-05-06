0ecf485ecc61bd2ff3986295da9f477e978cb139
Matt <mttwlkr@gmail.com>
2021-05-05 23:07:31 -0600

    Add countdown

--- components/CardCountdown.tsx
+++ components/CardCountdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Black35SansBold } from "./Text";
 
@@ -9,8 +9,38 @@ const CountdownWrapper = styled.div`
   text-align: center;
 `;
 
+const zeroPad = (num, places) => String(num).padStart(places, "0");
+
+function useCountdownTime() {
+  const [timeLeft, setTimeLeft] = useState("00:00:00:00");
+  const endsAt = 1620302400 * 1000;
+
+  let intervalId = setInterval(function () {
+    const now = Date.now();
+    const diff = endsAt - now;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    const pDay = zeroPad(days, 2);
+    const pHours = zeroPad(hours, 2);
+    const pMin = zeroPad(minutes, 2);
+    const pSec = zeroPad(seconds, 2);
+
+    setTimeLeft(() => `${pDay}:${pHours}:${pMin}:${pSec}`);
+  }, 1000);
+
+  useEffect(() => {
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return { timeLeft };
+}
+
 export const CardCountdown: React.FC = () => {
-  const [timeLeft, setTimeLeft] = useState("00:10:34:04");
+  const { timeLeft } = useCountdownTime();
 
   return (
     <CountdownWrapper>
